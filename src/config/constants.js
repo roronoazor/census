d0d0004a29aa696970c1ca6c24ee76d538de7adf
roronoazor <ugoodumegwu@gmail.com>
2023-04-02 17:03:12 +0100

    feat: updated the api url

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,4 +2,4 @@ export const primaryColor = '#4507FE';
 export const secondaryColor = '#C2AFFD';
 
 
-export const API_URL = 'http://localhost:8000'
+export const API_URL = 'http://16.16.100.100/api'
